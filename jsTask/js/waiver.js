5a47d8f6ec7e270636f5d99f3a9fc2778ea67e48
THSort <23100056@lums.edu.pk>
2024-06-09 17:12:39 +0500

    commit

---
+++ jsTask/js/waiver.js
@@ -0,0 +1,61 @@
+const addOneThirdClassToFigures = () => {
+    var figures = document.querySelectorAll("figure");
+    figures.forEach((figure) => {
+      figure.classList.add("one-third");
+  
+      figure.addEventListener("dblclick", () => {
+        if (figure === figures[4]) {
+          figure.style.visibility = "hidden";
+        }
+      });
+    });
+}
+
+const handleFooterClick = () => {
+    var footer = document.querySelector("footer");
+    footer.addEventListener("click", () => {
+      footer.style.display = "none";
+    });
+}
+
+const handleFirstFigureMouseEvents = () => {
+    var firstFigureCaption = document.querySelector("figure:first-of-type figcaption");
+    var originalFont = window.getComputedStyle(firstFigureCaption).fontFamily;
+
+    firstFigureCaption.addEventListener("mouseenter", () => {
+        firstFigureCaption.style.fontFamily = "cursive";
+    });
+
+    firstFigureCaption.addEventListener("mouseleave", () => {
+        firstFigureCaption.style.fontFamily = originalFont;
+    });
+}
+
+const handleFirstFigureFocusEvents = () => {
+    var firstFigureCaption = document.querySelector("figure:first-of-type figcaption");
+
+    firstFigureCaption.setAttribute("tabindex", "0");
+
+    firstFigureCaption.addEventListener("focus", () => {
+        firstFigureCaption.style.fontFamily = "cursive";
+    });
+
+    firstFigureCaption.addEventListener("blur", () => {
+        firstFigureCaption.style.fontFamily = "Times";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // problem 1
+    addOneThirdClassToFigures(); 
+
+    // problem 2
+    handleFooterClick(); 
+
+    // problem 3
+    handleFirstFigureMouseEvents(); 
+
+    // problem 4
+    handleFirstFigureFocusEvents(); 
+  });
+  
